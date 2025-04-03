94934c07f4aa900a0f68713a49c16b2a0808c6b5
JRTolley <JackRTolley@gmail.com>
2025-04-03 16:22:29 +0700

    Basic route stubs

--- index.js
+++ index.js
@@ -17,13 +17,26 @@ const PRIVATE_APP_ACCESS = '';
 
 // * Code for Route 1 goes here
 
+app.get("/", async (req, res) => {
+
+})
+
+
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
 
+app.get("/update-cobj", async (req, res) => {
+
+})
+
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+
+app.post("/update-cobj", async (req, res) => {
+
+})
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
